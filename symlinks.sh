7ef09bc62971ce794b383effdfc44524f43a2220
7555G <630r63.7555@gmail.com>
2018-01-07 14:23:44 +0200

    Create symlinks.sh

---
+++ symlinks.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+
+#
+# ~/.macos-dotfiles/symlinks.sh
+#
+
+
+# make soft symlinks
+ln -sfv ~/.macos-dotfiles/inputrc      ~/.inputrc
+ln -sfv ~/.macos-dotfiles/bash_profile ~/.bash_profile
+ln -sfv ~/.macos-dotfiles/vimrc        ~/.vimrc
+ln -sfv ~/.macos-dotfiles/latexmkrc    ~/.latexmkrc
