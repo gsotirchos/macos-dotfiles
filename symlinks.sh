1e2633d34418ba3c8dcf276c044ddddaccd7a177
Giorgos Sotirchos <630r63.7555@gmail.com>
2018-05-13 15:33:02 +0300

    add ranger dotfiles

--- symlinks.sh
+++ symlinks.sh
@@ -4,9 +4,14 @@
 # ~/.macos-dotfiles/symlinks.sh
 #
 
+# prepare folders
+mkdir -vp ~/.config/ranger/colorschemes
 
 # make soft symlinks
 ln -sfv ~/.macos-dotfiles/inputrc      ~/.inputrc
 ln -sfv ~/.macos-dotfiles/bash_profile ~/.bash_profile
 ln -sfv ~/.macos-dotfiles/vimrc        ~/.vimrc
 ln -sfv ~/.macos-dotfiles/latexmkrc    ~/.latexmkrc
+
+ln -sfv ~/.macos-dotfiles/config/ranger/*.*                ~/.config/ranger
+ln -sfv ~/.macos-dotfiles/config/ranger/colorschemes/*     ~/.config/ranger/colorschemes
