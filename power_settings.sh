4c6a4403e2b649e099d71acc526d4fb7f704969f
Giorgos Sotirchos <630r63.7555@gmail.com>
2018-11-30 20:54:38 +0200

    Reduce standby to 1 hr

--- power_settings.sh
+++ power_settings.sh
@@ -2,9 +2,9 @@
 
 # set some preffered power settings
 sudo pmset -a standby 1         # enable safe slep (hibernate)
-sudo pmset -a hibernatemode 3    # RAM on sleep, disk on safe sleep
-sudo pmset -a standbydelay 5400 # sleep for 1.5 hours before safe sleep
+sudo pmset -a hibernatemode 3   # use RAM on sleep, disk on safe sleep
+sudo pmset -a standbydelay 3600 # sleep for 1 hour before  safe sleep
 sudo pmset -a autorestart 1     # reboot on poert loss
 sudo pmset -a autopoweroff 0    # disable mandatory safe sleep
 
-echo "power settings modified"
+echo "Power settings modified"
