1d82ea81e82d6fbc52e0bf4665fff1ce87ad54f7
Giorgos Sotirchos <630r63.7555@gmail.com>
2019-09-22 12:34:39 +0300

    Auto-determine dofiles path

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/opt/pkg/bin/bash
 
 #
-# ~/.macos-dotfiles/symlinks.sh
+# ${DOTFILES}/symlinks.sh
 #
 
 # dotfiles path
-DOTFILES="/Users/$USER/.macos-dotfiles"
+DOTFILES="$( dirname $( grealpath "$0" ) )"
 
 # prepare folders
 mkdir -vp ~/.config/ranger/colorschemes
@@ -13,13 +13,13 @@ mkdir -vp ~/.vim/undo
 mkdir -vp ~/.vim/after/syntax
 
 # make soft symlinks
-ln -sfv $DOTFILES/bin/*                        ~/.bin
-ln -sfv $DOTFILES/inputrc                      ~/.inputrc
-ln -sfv $DOTFILES/bash_profile                 ~/.bash_profile
-ln -sfv $DOTFILES/vimrc                        ~/.vimrc
-ln -sfv $DOTFILES/vim/after/syntax/tex.vim     ~/.vim/after/syntax/tex.vim
-ln -sfv $DOTFILES/config/ranger/*.*            ~/.config/ranger
-ln -sfv $DOTFILES/config/ranger/colorschemes/* ~/.config/ranger/colorschemes
-ln -sfv $DOTFILES/LaunchAgents/com.*           ~/Library/LaunchAgents
+ln -sfv ${DOTFILES}/bin/*                    ~/.bin
+ln -sfv ${DOTFILES}/inputrc                  ~/.inputrc
+ln -sfv ${DOTFILES}/bash_profile             ~/.bash_profile
+ln -sfv ${DOTFILES}/vimrc                    ~/.vimrc
+ln -sfv ${DOTFILES}/vim/after/syntax/tex.vim ~/.vim/after/syntax/tex.vim
+#ln -sfv ${DOTFILES}/config/ranger/*.*            ~/.config/ranger
+#ln -sfv ${DOTFILES}/config/ranger/colorschemes/* ~/.config/ranger/colorschemes
+ln -sfv ${DOTFILES}/LaunchAgents/com.*       ~/Library/LaunchAgents
 
 echo -e "\nDon't forget to load the .plist files!"
