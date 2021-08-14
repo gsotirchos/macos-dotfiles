b34bbf067393d05c032aed8610435f251003e0d5
7555G <630r63.7555@gmail.com>
2021-08-14 19:53:08 +0300

    Silence echo

--- etc/source_dirs_list.sh
+++ etc/source_dirs_list.sh
@@ -5,7 +5,7 @@ dirs_list="$1"
 main () {
     for completions_dir in $@; do
         for completion_file in "${completions_dir}"/*; do
-            echo "${completion_file}"
+            #echo "${completion_file}"
             source "${completion_file}"
         done
     done
