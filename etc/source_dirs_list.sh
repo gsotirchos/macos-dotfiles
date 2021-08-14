3be155cb9f3dc0fec0ec298d9c63c45bdf9e9922
7555G <630r63.7555@gmail.com>
2021-08-14 20:31:55 +0300

    Add check if exists before sourcing

--- etc/source_dirs_list.sh
+++ etc/source_dirs_list.sh
@@ -4,10 +4,12 @@ dirs_list="$1"
 
 main () {
     for completions_dir in $@; do
-        for completion_file in "${completions_dir}"/*; do
-            #echo "${completion_file}"
-            source "${completion_file}"
-        done
+        if [[ -e "${completions_dir}" ]]; then
+            for completion_file in "${completions_dir}"/*; do
+                #echo "${completion_file}"
+                source "${completion_file}"
+            done
+        fi
     done
 }
 
