4aa7a7d7af32ad306cdcffdf7b594863194f9997
gsotirchos <gsotirchos@student.tudelft.nl>
2023-07-13 21:43:38 +0200

    Add script for setting up tex live on macOS

---
+++ etc/setup_texlive.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+brew install basictex
+sudo tlmgr update --self
+sudo tlmgr install \
+    latexmk \
+    babel-greek \
+    greek-fontenc \
+    cbfonts \
+    subfiles \
+    appendix \
+    siunitx \
+    cancel \
+    extarrows \
+    csquotes \
+    ebgaramond \
+    courierten \
+    fontaxes
