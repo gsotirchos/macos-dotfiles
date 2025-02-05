0c020d2db99fd4163b4ced3846b2ec77514b17d4
gsotirchos <gsotirchos@student.tudelft.nl>
2025-02-05 22:22:49 +0100

    Improve cleanup

--- etc/cleanup_1password_items.sh
+++ etc/cleanup_1password_items.sh
@@ -4,45 +4,69 @@ main() {
     vault="${1:-"Private"}"
     account="${2:-"HQRXQK6P2FAE7NAAUUQF4ILVG4"}"
     declare -A itemMap
-    local id item fields username title urls href website key
+    local id item fields username title fixed_title urls href website key
 
     for id in $(op item list --categories Login --vault "$vault" --account "$account" --format=json | jq -r '.[] | select(.id != null) | .id'); do
         item=$(op item get "$id" --vault "$vault" --account "$account" --format=json)  # TIME consuming
+        if [[ $item == null ]]; then
+            echo "$id has no items; skipped"
+            continue
+        fi
 
-        if [[ $item != null ]]; then
-            fields=$(echo "$item" | jq -r '.fields')
-            if [[ $fields != null ]]; then
-                username=$(echo "$fields" | jq -r '.[] | select(.label=="username").value')
-            fi
-            if [[ $username == http* ]]; then
-              op item delete "$id" --archive --vault "$vault" --account "$account"
-              echo "$id deleted (url in username)"
-            fi
+        fields=$(echo "$item" | jq -r '.fields')
+        if [[ $fields == null ]]; then
+            echo "$id has no fields; skipped"
+            continue
+        fi
 
-            title=$(echo "$item" | jq -r '.title')
-            if [[ "$title" =~ \(.*\)$ ]]; then
-                fixed_title="${title% (*)}"
-                op item edit "$id" --vault "$vault" --account "$account" .title="$fixed_title" > /dev/null
-                echo "$id fixed title from '$title' to '$fixed_title'"
-            fi
+        # delete items with URLs as usernames
+        username=$(echo "$fields" | jq -r '.[] | select(.label=="username").value')
+        if [[ $username == http* ]]; then
+          op item delete "$id" --archive --vault "$vault" --account "$account"
+          echo "$id deleted (url in username)"
+          continue
+        fi
 
-            urls=$(echo "$item" | jq -r '.urls')
-            href=$(echo "$urls" | jq -r '.[0].href')
-            website=$(echo "$href" | awk -F[/:] '{print $4}')
-            if [[ -n $website && -n $username ]]; then
-                key="$website-$username"
-
-                if [[ ${itemMap[$key]} ]]; then
-                    echo "Duplicate found:"
-                    echo "  Item 1: id: ${itemMap[$key]}, username: $username, website: $website"
-                    echo "  Item 2: id: $id, username: $username, website: $website"
-                    op item delete "$id" --archive --vault "$vault" --account "$account"
-                    echo "$id deleted"
-                else
-                    itemMap[$key]=$id
-                    echo "$id added to itemMap"
-                fi
+        # delete duplicate items
+        urls=$(echo "$item" | jq -r '.urls')
+        href=$(echo "$urls" | jq -r '.[0].href')
+        website=$(echo "$href" | awk -F[/:] '{print $4}')
+        if [[ -n $website && -n $username ]]; then
+            key="$website-$username"
+            if [[ ${itemMap[$key]} ]]; then
+                echo "Duplicate found:"
+                echo "  Item 1: id: ${itemMap[$key]}, username: $username, website: $website"
+                echo "  Item 2: id: $id, username: $username, website: $website"
+                op item delete "$id" --archive --vault "$vault" --account "$account"
+                echo "$id deleted"
+                continue
+            else
+                itemMap[$key]=$id
+                echo "$id added to itemMap"
             fi
+        fi
+
+        title=$(echo "$item" | jq -r '.title')
+        fixed_title="$title"
+
+        # remove "...(username)" from title
+        if [[ "$fixed_title" =~ \(.*\)$ ]]; then
+            fixed_title="${fixed_title% (*)}"
+        fi
+
+        # use the full website name
+        if [[ "$fixed_title" != *.* ]]; then
+            fixed_title="$website"
+        fi
+
+        # remove "www." prefix if it exists
+        if [[ "$fixed_title" == "www."* ]]; then
+            fixed_title="${fixed_title#www.}"
+        fi
+
+        if [[ "$title" != "$fixed_title" ]]; then
+            op item edit "$id" --vault "$vault" --account "$account" .title="$fixed_title" > /dev/null
+            echo "$id fixed title from '$title' to '$fixed_title'"
         fi
     done
 }
