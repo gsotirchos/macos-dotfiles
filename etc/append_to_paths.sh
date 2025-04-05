36e5853d702c74821a51309073602509579127f7
gsotirchos <gsotirchos@student.tudelft.nl>
2025-04-05 21:24:06 +0200

    Fix broken behavior

--- etc/append_to_paths.sh
+++ etc/append_to_paths.sh
@@ -6,58 +6,60 @@
 main() {
     local extra_paths_dir
     extra_paths_dir="$(realpath "$1")"
+    local -A modified_env_vars
 
     for extra_paths_file in "${extra_paths_dir}"/*; do
-        # Only process regular files (the path definition files)
         if [[ -f "$extra_paths_file" ]]; then
-            local global_var_name
-            global_var_name="$(basename "${extra_paths_file}")"
-            global_var_name="${global_var_name%%.*}"
+            local env_var_name
+            env_var_name="$(basename "${extra_paths_file}")"
+            env_var_name="${env_var_name%%.*}"
 
             local extra_paths
             readarray -t extra_paths < <(envsubst < "${extra_paths_file}")  # expand env vars in paths
 
-            # append paths to global variable
-            append_paths "$global_var_name" "${extra_paths[@]}"
+            modified_env_vars["${env_var_name}"]="$(append_paths "$env_var_name" "${extra_paths[@]}")"
         fi
     done
+
+    for env_var_name in "${!modified_env_vars[@]}"; do
+        export "${env_var_name}=${modified_env_vars["$env_var_name"]}"
+    done
 }
-# append paths to global variable
-# input argument 1: the global variable name
-# input argument 2...: the paths to be appended
+
+# append paths to env variable
+# input argument 1: the env variable name
+# input arguments 2...: the paths to be appended
 append_paths() {
-    local global_var_name="$1"
+    local env_var_name="$1"
     shift
-
     local paths_to_add=($(eval echo \"$@\"))  # expand widlcards
-    local -A added_paths  # Use an associative array as a set
-
-    # Populate the set with existing paths (for faster lookup)
-    IFS=':' read -ra existing_paths <<< "${!global_var_name}"
+    local -A added_paths
+    IFS=':' read -ra existing_paths <<< "${!env_var_name}"
     for path in "${existing_paths[@]}"; do
         if [[ "${path}" ]]; then
-            #echo "adding existing: $path"
+            #echo "existing path: $path"
             added_paths["$path"]=1
         fi
     done
-
-    local new_paths_array=()  # array >>> than string!
-    new_paths_array+=("${!global_var_name}")
+    local new_paths_array=()
 
     for path in "${paths_to_add[@]}"; do
         if [[ "${added_paths["$path"]}" ]]; then
+            #echo "Warning: skipped adding existing path \"$path\" in $env_var_name"
             continue
         fi
         if [[ "${path:0:1}" == "#" ]]; then
-            #echo "skipped: $path"
+            #echo "skipped invalid: $path"
             continue
         fi
-        #echo -e "appended to $global_var_name: $path"
+        #echo -e "appended to $env_var_name: $path"
         new_paths_array+=("$path")
         added_paths["$path"]=1
     done
 
-    IFS=":" export "$global_var_name=${new_paths_array[*]}"  # 😗👌
+    new_paths_array+=("${!env_var_name}")
+    local IFS=":"
+    echo "${new_paths_array[*]}"
 }
 
 main "$@"
