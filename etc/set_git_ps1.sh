e9b21bd6c115d4e13b4f7a6a1659e42f8c1ad29e
gsotirchos <gsotirchos@student.tudelft.nl>
2025-04-05 18:56:33 +0200

    Cleanup

--- etc/set_git_ps1.sh
+++ etc/set_git_ps1.sh
@@ -1,8 +1,8 @@
 # make git prompt command available
 if ! command -v "__git_ps1" &> /dev/null; then
     git_prompt_path="/etc/bash_completion.d/git-prompt"
-    if [[ -f "/opt/homebrew/${git_prompt_path}.sh" ]]; then
-        source "/opt/homebrew/${git_prompt_path}.sh"
+    if [[ -f "${HOMEBREW_PREFIX}/${git_prompt_path}.sh" ]]; then
+        source "${HOMEBREW_PREFIX}/${git_prompt_path}.sh"
     elif [[ -f "${git_prompt_path}" ]]; then
         source "${git_prompt_path}"
     fi
--- etc/source_dirs_list.sh
+++ etc/source_dirs_list.sh
@@ -1,5 +1,11 @@
 # shellcheck disable=SC1090
 
+# DEPRECATED
+# Use with:
+#   if [[ -f "${dotfiles}"/completion_dirs ]]; then
+#       source "${macos_dotfiles}"/etc/source_dirs_list.sh "${dotfiles}"/completion_dirs
+#   fi
+
 # the main function sources a list of files from file contents
 # input argument: the file containing the locations of the files to be sourced
 main() {
@@ -7,7 +13,7 @@ main() {
     readarray -t dirs_list < <(envsubst < "$@")
 
     for completions_dir in "${dirs_list[@]}"; do
-        if [[ -e "${completions_dir}" ]]; then
+        if [[ -e "${completions_dir}" ]]; then  # TIME ~200s
             for completion_file in "${completions_dir}"/*; do
                 #echo "${completion_file}"
                 source "${completion_file}"
