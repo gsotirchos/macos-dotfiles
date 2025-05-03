2e0ff34addfa2a84fd4436ff683d4e6ab30111cb
gsotirchos <gsotirchos@student.tudelft.nl>
2025-05-03 20:15:38 +0200

    Update emacs config heavily

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,14 +1,41 @@
 (set-face-attribute 'default nil :family "Menlo" :height 140)
-(load-theme 'tango-dark)
+
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+
+(defun my/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light (load-theme 'modus-operandi)
+	    (ns-appearance . light))
+    ('dark (load-theme 'modus-vivendi-tinted t)
+	    (ns-appearance . dark))))
+
+(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
+(setq modus-themes-common-palette-overrides
+      '((fringe unspecified)))
 
 
-(setq inhibit-startup-message t)
-(scroll-bar-mode -1)
+(unless (eq system-type 'darwin)
+  (menu-bar-mode -1)
+  (tooltip-mode -1)
+  (setq visible-bell t))
 (tool-bar-mode -1)
-;(menu-bar-mode -1)  ; except for macOS
-;(tooltip-mode -1)
-;(set-fringe-mode 8)
-;(setq visible-bell t)
+(scroll-bar-mode -1)
+(setq inhibit-startup-message t)
+(setq vc-follow-symlinks t)
+
+
+(column-number-mode)
+;(global-display-line-numbers-mode t)
+
+; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+		eshell-mode-hook
+		treemacs-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 
 ; Initialize package sources
@@ -22,15 +49,12 @@
 (unless package-archive-contents
   (package-refresh-content))
 
-  ;; Initialize use-package on non-Linux platforms
+; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (use-package ivy
   :diminish
@@ -50,28 +74,162 @@
   :config
   (ivy-mode 1))
 
+(use-package ivy-rich
+  :after (ivy counsel)
+  :init
+  (ivy-rich-mode 1))
+
 (use-package counsel
-  :bind (("C-M-j" . 'counsel-switch-buffer)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :custom
-  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  ;:bind (("C-M-j" . 'counsel-switch-buffer)
+  ;       :map minibuffer-local-map
+  ;       ("C-r" . 'counsel-minibuffer-history))
+  ;:custom
+  ;(counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
   :config
+  (setq ivy-initial-inputs-alist nil)  ; Don't start searches with ^
   (counsel-mode 1))
 
+(use-package ivy-prescient
+  :after counsel
+  :custom
+  (ivy-prescient-enable-filtering nil)
+  :config
+  ;; Uncomment the following line to have sorting remembered across sessions!
+  ;(prescient-persist-mode 1)
+  (ivy-prescient-mode 1))
+
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 1))
+
+; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+; Disable Ctrl-Z
+(global-set-key (kbd "C-z") nil)
+
+
+(use-package general
+  :after evil
+  :config
+  (general-create-definer efs/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+  (efs/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")
+    "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/init.el")))))
+
+;(use-package hydra
+;  :defer t)
+;
+;(defhydra hydra-text-scale (:timeout 4)
+;  "scale text"
+;  ("j" text-scale-increase "in")
+;  ("k" text-scale-decrease "out")
+;  ("f" nil "finished" :exit t))
+;
+;(efs/leader-keys
+;  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+
+;(use-package doom-themes
+;  :init (load-theme 'doom-palenight t))
+
+(use-package nerd-icons)
+
 (use-package doom-modeline
-  :ensure t
   :init (doom-modeline-mode 1))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+
+(use-package no-littering)
+
+; no-littering doesn't set this by default so we must place
+; auto save files in the same path as it uses for sessions
+(setq auto-save-file-name-transforms
+      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Projects/Code")
+    (setq projectile-project-search-path '("~/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :after projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :commands magit-status  ; probably unnecessary
+  ;:custom
+  ;(magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
+)
+
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+;(use-package forge
+;  :after magit)
+
+(use-package evil-nerd-commenter
+  :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+
+
+;;; Offload the custom-set-variables to a separate file
+(setq custom-file "~/.emacs.d/custom.el")
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+;;; Load custom file. Don't hide errors. Hide success message
+(load custom-file nil t)
