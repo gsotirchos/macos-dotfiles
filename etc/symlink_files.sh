e826d130fb268a4e9cfaa2e2d270301774e8aadb
gsotirchos <gsotirchos@student.tudelft.nl>
2025-10-25 09:37:01 +0200

    Backup original dotfiles

--- etc/symlink_files.sh
+++ etc/symlink_files.sh
@@ -1,9 +1,15 @@
 target_dir="$(realpath "$1")"
-destination_prefix="$2"
+destination_dir="$2"
 
 # make soft symlinks of files
 for filename in "${target_dir}"/*; do
     if [[ -f "${filename}" ]]; then
-        ln -sfv "${filename}" "${destination_prefix}$(basename "${filename}")"
+        destination_filepath="${destination_dir}/$(basename "${filename}")"
+        # backup existing files first
+        if [[ -e "${destination_filepath}" ]] && [[ ! -L "${destination_filepath}" ]]; then
+            mkdir -p "${destination_dir}/backup"
+            cp -rf "${destination_filepath}" "${destination_dir}/backup"
+        fi
+        ln -sfv "${filename}" "${destination_filepath}"
     fi
 done
