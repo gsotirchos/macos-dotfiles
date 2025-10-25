9181ad337c0754d92480c863943184e56d89b891
gsotirchos <gsotirchos@student.tudelft.nl>
2025-10-25 10:48:22 +0200

    Fix conda dir finding

--- conda/conda_init.sh
+++ conda/conda_init.sh
@@ -2,17 +2,26 @@
 # ~/.conda/conda_init.sh
 #
 
-case "$(uname -s)" in
-    Linux*)
-        conda_path="/opt/miniforge" # NOTE: custom location
-        ;;
-    Darwin*)
-        conda_path="${HOMEBREW_PREFIX}/Caskroom/miniforge/base"
-        ;;
-esac
+# shellcheck disable=SC1091
+
+
+potential_paths=(
+    "$(realpath "$(dirname "${CONDA_EXE}")/..")"
+    "/opt/miniforge"
+    "${HOMEBREW_PREFIX}/Caskroom/miniforge/base"
+)
+
+for path in "${potential_paths[@]}"; do
+  if [[ -x "${path}/bin/conda" ]]; then
+    conda_path="$path"
+    break
+  fi
+done
+unset potential_paths
 
 if conda_setup="$("${conda_path}/bin/conda" "shell.bash" "hook" 2> /dev/null)"; then
     eval "${conda_setup}"
+    unset conda_setup
 else
     if [ -f "${conda_path}/etc/profile.d/conda.sh" ]; then
         source "${conda_path}/etc/profile.d/conda.sh"
