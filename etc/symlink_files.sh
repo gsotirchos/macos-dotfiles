5844407f33dcfd0aa1df29ce49f44a55c85f7ecb
gsotirchos <gsotirchos@student.tudelft.nl>
2025-10-25 09:52:06 +0200

    Fix target links' filenames

--- etc/symlink_files.sh
+++ etc/symlink_files.sh
@@ -1,10 +1,10 @@
 target_dir="$(realpath "$1")"
-destination_dir="$2"
+destination_dir="$(realpath "$2")"
 
 # make soft symlinks of files
 for filename in "${target_dir}"/*; do
     if [[ -f "${filename}" ]]; then
-        destination_filepath="${destination_dir}/$(basename "${filename}")"
+        destination_filepath="${destination_dir}/.$(basename "${filename}")"
         # backup existing files first
         if [[ -e "${destination_filepath}" ]] && [[ ! -L "${destination_filepath}" ]]; then
             mkdir -p "${destination_dir}/backup"
